287bdf5506eb40d73c6d4811ac826c76708b704e
Rafael Salinas Gonzalez <rafsalgon@gmail.com>
2024-12-14 14:13:52 +0000

    Counter

---
+++ src/js/component/Home.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Card from "./Counter";
+
+
+
+export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,44 @@
 //import react into the bundle
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import PropTypes, { number } from "prop-types";
 
-//import your own components
-import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
+let counter = 0
 
+
+const Counter = (props) => {
+  return (<div className="counterbox">
+    <div className="m-3"><i className="fa-solid fa-clock"></i></div>
+    <div className="six m-1">{props.six}</div>
+    <div className="five m-1">{props.five}</div>
+    <div className="four m-1">{props.four}</div>
+    <div className="three m-1">{props.three}</div>
+    <div className="two m-1">{props.two}</div>
+    <div className="one m-1">{props.one}</div>
+  </div>
+  )
+}
+
+Counter.propTypes = {
+  one: PropTypes.number,
+  two: PropTypes.number,
+  three: PropTypes.number,
+  four: PropTypes.number,
+  five: PropTypes.number,
+  six: PropTypes.number,
+}
+
+setInterval(() => {
+  ReactDOM.createRoot(document.getElementById("app")).render(
+    <React.StrictMode>
+      <Counter one={counter} />
+    </React.StrictMode>,
+  )
+  counter = counter + 1;
+}, 1000);
